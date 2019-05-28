a0bf322e05a7a858f7f15e36c452e375ff0acb51
Fred Valle <phredvalle@gmail.com>
2019-05-28 11:38:58 -0500

    fixed bug, showing duplicate links exported, need to use both class names in cheerio selector

--- scrape2.js
+++ scrape2.js
@@ -1,11 +1,21 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const fs = require('fs');
+
+// Start streamin to csv file
+const writeStream = fs.createWriteStream('post.csv');
+
+// Write headers
+// use backticks to avoid using concatenation
+writeStream.write(`Title,Link,Date \n`);
+
+
 
 request("https://johnpapa.net/", (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
 
-    $(".post_head").each((i, el) => {
+    $(".post_head.post").each((i, el) => {
       const title = $(el)
         .find(".post_title")
         .text()
@@ -20,10 +30,13 @@ request("https://johnpapa.net/", (error, response, html) => {
 
       const date = $(el)
         .find("time")
-        .text();
+        .text();     
+      //console.log(title, link, date);
 
-      //console.log(date);
-      console.log(title, link, date);
+    // Write to csv
+    writeStream.write(`${title}, ${link}, ${date} \n`);
     });
   }
+
+  console.log('Scraping done....');
 });
