8143d8b98b1260e6f6e6d9e957f2bc689868c7dd
Fred Valle <phredvalle@gmail.com>
2019-05-28 09:04:19 -0500

    first request to a blog site

---
+++ scrape.js
@@ -0,0 +1,11 @@
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+request('https://johnpapa.net/', (error, response, html) => {
+
+    if (!error && response.statusCode == 200) { 
+        console.log(html);
+    }
+
+});
